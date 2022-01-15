26a294463f9cd44b798afc600b7a342ae002c37f
Stephan Kulow <coolo@suse.de>
2022-01-15 22:10:20 +0100

    Redo the finger print math

--- pick.py
+++ pick.py
@@ -3,6 +3,7 @@
 import numpy as np
 from numpy.core.numeric import indices
 from sklearn.cluster import KMeans
+import os
 
 incides = []
 for i in range(ord('a'), ord('z')+1):
@@ -19,19 +20,25 @@ best_hit=None
 def array_to_str(array):
     str=""
     for x in array:
-        str += chr(int(x * 10 + 0.5) + ord('0'))
+        str += chr(x + ord('0'))
     return str
 
 over=np.zeros((1, 36), dtype=np.float32)
+data = np.load('all-waves.npz', mmap_mode='r')['x']
+length=data[0].shape[0]
+over=np.zeros((length * len(list(data)), 36), dtype=np.float32)
+for i, x in enumerate(data):
+    over[i*length:(i+1)*length,0:36] = x
+# map to -1 .. -1
+over = np.clip(over + 10, -10, 10) / 10
+import matplotlib.pyplot as plt
+mu = 10
+# map to 0..1
+over = (np.sign(over) * np.log(1.0 + mu * np.abs(over)) / np.log(1.0 + mu) + 1) / 2
 
-with open('labels_in.txt', 'r') as f:
-    for line in f:
-        array=[ord(i)-ord('0') for i in line.strip()]
-        array = np.array(array, dtype=np.float32) / 10
-        over = np.vstack((over, array))
-
-kmeans = KMeans(n_clusters=len(incides), random_state=0, verbose=1).fit(over)
-centers = kmeans.cluster_centers_
+kmeans = KMeans(n_clusters=len(incides), random_state=0, verbose=1, max_iter=1).fit(over)
+centers = np.int8(kmeans.cluster_centers_ * 10)
+centers = np.unique(centers, axis=0)
 for i, k in enumerate(centers):
     line = array_to_str(k)
     print(f"'{incides[i]}': np.array([" + ",".join([i for i in line.strip()]) + "], dtype=np.float32) / 10,")
